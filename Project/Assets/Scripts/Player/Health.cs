f555f53a2cd6de458a401eacdb52bd3dc6f46329
Asigaka(Nikita) <Asigakasaramadzai@yandex.ru>
2022-07-23 11:26:04 +0300

    health+

--- Project/Assets/Scripts/Player/Health.cs
+++ Project/Assets/Scripts/Player/Health.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float currentHealth;
+
+    [HideInInspector] public UnityEvent onDamage;
+    [HideInInspector] public UnityEvent onDie;
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Damage(float damage)
     {
-        
+        currentHealth -= damage;
+        onDamage.Invoke();
+
+        if (currentHealth <= 0)
+        {
+            onDie.Invoke();
+        }
     }
 }
